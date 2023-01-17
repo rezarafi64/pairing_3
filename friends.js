0ce1af4d445f87497b92daaa25f5b13c017b0b1c
Reza <rezarafi64@gmail.com>
2023-01-16 19:11:21 -0700

    Log nums under 60

--- friends.js
+++ friends.js
@@ -1,6 +1,12 @@
 var nums = [1, 8, 16, 24, 32, 40, 48, 56]
+// for (var i = 0; i < nums.length; i++) {
+//     // if (nums[i] > 30) {
+//         console.log("This is your magic number " + nums[i] + " Anna!")
+//     // }
+// }
+
 for (var i = 0; i < nums.length; i++) {
-    // if (nums[i] > 30) {
-        console.log("This is your magic number " + nums[i] + " Anna!")
-    // }
+    if (nums[i] < 60) {
+        console.log ("Number " + nums[i] + " is manifested by Gods!")
+    }
 }
