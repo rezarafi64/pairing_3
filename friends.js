3d454b5cb9d5d75ee68fef98232f7daab4ab9927
Reza <rezarafi64@gmail.com>
2023-01-16 18:56:05 -0700

    Create friends numbers

---
+++ friends.js
@@ -0,0 +1,6 @@
+var nums = [1, 8, 16, 24, 32, 40, 48, 56]
+for (var i = 0; i < nums.length; i++) {
+    // if (nums[i] > 30) {
+        console.log("This is your magic number " + nums[i] + " Anna!")
+    // }
+}
