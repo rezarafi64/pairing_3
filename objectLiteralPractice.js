c0ff183e2a0e0e92319ab23b0e96878edd2581a3
Reza <rezarafi64@gmail.com>
2023-01-16 18:04:37 -0700

    Create person object

---
+++ objectLiteralPractice.js
@@ -0,0 +1,14 @@
+var person = {
+    name: "Princess Consuela",
+    location: "Vegas",
+    age: 25,
+    isMarried: false
+}
+
+console.log(person)
+console.log(person.name)
+
+person.race = "White"
+person.pets = ["Ramona", "Koko", "Mali"]
+
+console.log(Object.keys(person))
